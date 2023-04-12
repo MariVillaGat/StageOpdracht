6e808755e20e751f56f62146312be367a158e4c0
MARI_VILLALOBOS_GAT <marivillalobos93cr@gmail.com>
2023-04-12 11:34:26 +0200

    change

--- database/migrations/2023_03_28_213743_create_products_table.php
+++ database/migrations/2023_03_28_213743_create_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
             $table->string('title');
             $table->string('logo')->nullable();
             $table->string('tags');
