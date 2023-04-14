cfdbd5f887c1f7eb7308e6d72a1d010c9a3acafd
MARI_VILLALOBOS_GAT <marivillalobos93cr@gmail.com>
2023-04-14 10:27:09 +0200

    changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -76,7 +76,7 @@
     @yield('content')
     </main>
     <footer
-    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-20 mt-24 opacity-90 md:justify-center"
 >
     <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
